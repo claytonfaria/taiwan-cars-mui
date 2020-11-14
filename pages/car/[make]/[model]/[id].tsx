c7aa3d2532dcc13d25d827ffa4569f73fa71b647
Clayton Faria <claytonfaria@gmail.com>
2020-11-14 19:39:21 +0800

    added blur to images in the car details

--- pages/car/[make]/[model]/[id].tsx
+++ pages/car/[make]/[model]/[id].tsx
@@ -2,9 +2,11 @@
 import { Button, Grid, Paper, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import type { GetStaticPaths, GetStaticProps } from 'next';
+import { getBlurhash } from 'next-blurhash';
 import Head from 'next/head';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { BlurhashCanvas } from 'react-blurhash';
 
 import { getAllCars } from '../../../../lib/getAllCars';
 import type { CarModel } from '../../../../models/Car';
@@ -25,9 +27,10 @@ const useStyles = makeStyles((theme) => ({
 
 type CarDetailsProps = {
   car: CarModel;
+  imgHash: string;
 };
 
-export default function CarDetails({ car }: CarDetailsProps) {
+export default function CarDetails({ car, imgHash }: CarDetailsProps) {
   const classes = useStyles();
   const router = useRouter();
 
@@ -49,7 +52,31 @@ export default function CarDetails({ car }: CarDetailsProps) {
       <Paper className={classes.paper} elevation={3}>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6} md={5}>
-            <Image alt="complex" height={368} width={491} src={car.photourl} />
+            <div style={{ position: 'relative' }}>
+              <BlurhashCanvas
+                // Your generated Blurhash string
+                hash={imgHash}
+                // getBlurhash **always** returns 32×32 dimensions
+                width={32}
+                height={32}
+                punch={1}
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  width: '100%',
+                  height: '100%',
+                }}
+              />
+              <Image
+                alt="complex"
+                height={368}
+                width={491}
+                src={car.photourl}
+              />
+            </div>
           </Grid>
           <Grid item xs={12} sm={6} md={7} container>
             <Grid item xs container direction="column" spacing={2}>
@@ -101,9 +128,10 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   }).lean()) as CarModel;
 
   delete car._id;
+  const imgHash = await getBlurhash(car.photourl);
 
   return {
-    props: { car },
+    props: { car, imgHash },
     revalidate: 1000,
   };
 };
